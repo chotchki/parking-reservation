177b643a4c0ac69c924a65bcccbcc6a15fc89c87
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2014-09-20 19:38:33 -0400

    Switched to using a directive

--- app.js
+++ app.js
@@ -18,5 +18,16 @@
 	]; 
 	app.controller('ParkingController', function(){
 		this.spaces = spaces;
+
+		this.addSpace = function(){
+			this.spaces.push({id: (this.spaces.length), purchased: false});
+		};
+	});
+
+	app.directive('parkingSpace', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'parking-space.html'
+		};
 	});
 })();
